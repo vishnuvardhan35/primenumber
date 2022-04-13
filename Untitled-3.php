b2423a234f44a73f722de66ac03f6f66a4894457
vishnuvardhan35 <102521683+vishnuvardhan35@users.noreply.github.com>
2022-04-13 17:09:24 +0530

    Add files via upload

---
+++ Untitled-3.php
@@ -0,0 +1,55 @@
+<table>
+<tr>
+    <th>country name</th>
+    <th>population</th>
+    <th>area</th>
+</tr>
+<tr>
+    <td>India</td>
+    <td>1380.0m</td>
+    <td>3,287,300km</td>
+</tr>
+<tr>
+    <td> america</td>
+    <td>329.5m</td>
+    <td>9,831,500km</td>
+</tr>
+<tr>
+    <td>china</td>
+    <td>1410.9m</td>
+    <td> 9,562,900km</td>
+</tr>
+<tr>
+    <td>pakistan</td>
+    <td>220.9m</td>
+    <td>796,100km</td>
+</tr>
+<tr>
+    <td> brazil</td>
+    <td> 212.6m</td>
+    <td> 8,515,800km</td>
+</tr>
+<tr>
+    <td> russia</td>
+    <td> 144.1m</td>
+    <td> 17,098,300km</td>
+</tr>
+<tr>
+    <td> mexico</td>
+    <td> 128.9m</td>
+    <td> 1,964,400km</td>
+</tr>
+<tr>
+<td> japan</td>
+<td> 125.8m</td>
+<td>378,000km</td>
+</tr>
+<tr>
+    <td> iran</td>
+    <td> 84.0m</td>
+    <td>1,745,200km</td>
+</tr>
+<tr>
+    <td> germany</td>
+    <td> 83.2m</td>
+    <td> 357,600km</td>
